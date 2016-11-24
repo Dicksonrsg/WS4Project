cd8eead624b54b055dea765bf31e16e42e64c28b
Dickson <Dickson@Tankyu-sha>
2016-11-24 09:31:18 -0300

    24/11 2nd

--- Guardian/NetBeansProjects/PWebF/src/main/java/model/Soldier.java
+++ Guardian/NetBeansProjects/PWebF/src/main/java/model/Soldier.java
@@ -6,7 +6,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import org.apache.commons.lang3.text.WordUtils;
 
 @Entity(name = "tb_soldiers")
 public class Soldier implements PersistDB, Serializable {
@@ -61,7 +60,6 @@ public class Soldier implements PersistDB, Serializable {
     }
 
     public void setName(String name) {
-        WordUtils.capitalize(name);
         this.name = name;
     }
 
